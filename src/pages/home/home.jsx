0db466e4f4fe579712e27003910c95624ffd5ee1
Soleh.jr <mahmadievsoleh.jr@gmail.com>
2025-06-07 14:03:10 +0500

    edit home page and events

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { News } from '../../app/providers/lazy/lazy'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <News/>
+    </div>
   )
 }
 
